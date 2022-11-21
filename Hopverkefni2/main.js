cdae88ca38c7662d6558ebadb6e950c30aa737cd
Sandesh <63111343+sandesh1337@users.noreply.github.com>
2022-11-21 21:26:13 +0000

    added favicon and some code snippets
    added code snippets on main that will be useful later for url and stuff

--- Hopverkefni2/main.js
+++ Hopverkefni2/main.js
@@ -1,6 +1,28 @@
 import './style.css';
 import { empty } from './scripts/helperfunctions';
 
+const header = document.querySelector('.layout__header');
+const main = document.querySelector('.layout__main');
+
+// This commented line below is used to push the initial route
+/* window.history.pushState({}, '', `/?query=${value}`);  */
+
+/**
+ * Athugar hvaða síðu við erum á út frá query-string og birtir viðeigandi
+ * gögn. Ef `id` er gefið er bók birt, ef `query` er gefið er leitarniðurstaða
+ * birt, annars er forsíða birt.
+ */
+/*
+ function route() {
+  const params = new URLSearchParams(window.location.search);
+  const id = params.get('id');
+  const query = params.get('query');
+
+}
+*/
+/**
+ * Bregst við því þegar við notum vafra til að fara til baka eða áfram.
+ */
 window.onpopstate = () => {
   empty(main);
   route();
