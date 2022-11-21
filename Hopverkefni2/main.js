c8835f444f45e449b714e005def9935d43617434
Sandesh <63111343+sandesh1337@users.noreply.github.com>
2022-11-21 21:15:00 +0000

    added helper functions
    also trying to add history api functionality

--- Hopverkefni2/main.js
+++ Hopverkefni2/main.js
@@ -1,1 +1,10 @@
-import "./style.css";
+import './style.css';
+import { empty } from './scripts/helperfunctions';
+
+window.onpopstate = () => {
+  empty(main);
+  route();
+};
+
+// Athugum í byrjun hvað eigi að birta.
+route();
