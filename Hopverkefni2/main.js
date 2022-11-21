47b298eba036603c1115ac7a44b6ace79d6399d7
Sandesh <63111343+sandesh1337@users.noreply.github.com>
2022-11-21 23:43:12 +0000

    finally published data on overview page , styling remains

--- Hopverkefni2/main.js
+++ Hopverkefni2/main.js
@@ -1,5 +1,6 @@
 import './style.css';
-import { empty } from './scripts/helpers';
+import './data/events.json';
+import { empty, el } from './scripts/helpers';
 import {
   createSearchInput,
   fetchAndRenderEvents,
@@ -10,6 +11,33 @@ import {
 const header = document.querySelector('.layout__header');
 const main = document.querySelector('.layout__main');
 
+async function getData() {
+  try {
+    await fetch('./data/events.json')
+    .then((response) => response.json())
+    .then((json) => savedata(json));
+  } catch (error) {
+    console.log("error");
+  }
+}
+getData();
+/*
+data[i].language.is.title
+data[i].language.is.place
+data[i].language.is.text */
+function savedata(jsonfromfile) {
+  const data = jsonfromfile;
+  console.log(data);
+  for (let i = 0; i < data.length; i++) {
+    main.append(el('p', {}, data[i].language.is.title));
+    main.append(el('p', {}, data[i].language.is.place));
+    main.append(el('p', {}, data[i].language.is.text));
+    // append
+  }
+  
+}
+
+
 // This commented line below is used to push the initial route
 /* window.history.pushState({}, '', `/?query=${value}`);  */
 
@@ -18,14 +46,20 @@ const main = document.querySelector('.layout__main');
  * gögn. Ef `id` er gefið er bók birt, ef `query` er gefið er leitarniðurstaða
  * birt, annars er forsíða birt.
  */
-/*
- function route() {
+
+function route() {
   const params = new URLSearchParams(window.location.search);
   const id = params.get('id');
   const query = params.get('query');
-
+  if (id) {
+    fetchAndRenderEvents(main, searchForm, id);
+  } else if (query) {
+    searchAndRender(main, searchForm, query);
+  } else {
+    renderFrontpage(main);
+  }
 }
-*/
+
 /**
  * Bregst við því þegar við notum vafra til að fara til baka eða áfram.
  */
@@ -35,4 +69,4 @@ window.onpopstate = () => {
 };
 
 // Athugum í byrjun hvað eigi að birta.
-route();
+/*route();*/
